a3dc3a22417eb11550e9b05e8250ba73c59fc0b7
SimonLucas <simonmarklucas@gmail.com>
2024-03-08 15:50:44 +0000

    Update test_stats.py

--- pysrc/tests/test_stats.py
+++ pysrc/tests/test_stats.py
@@ -18,6 +18,12 @@ class TestStats(unittest.TestCase):
         stats_list.add(1)
         self.assertEqual(stats_list.get_mean(), 1)
 
+    def test_mean_2(self):
+        stats_list = StatsList()
+        stats_list.add(1)
+        stats_list.add(2)
+        self.assertEqual(stats_list.get_mean(), 1.5)
+
 
 if __name__ == '__main__':
     unittest.main()
